f2661f994936628a73fb023450a515dc55f43e96
113310220 <t113310220@ntut.org.tw>
2025-02-26 11:46:13 +0800

    Update Lab01_Ex1.cpp

--- Lab01/Lab01_Ex1.cpp
+++ Lab01/Lab01_Ex1.cpp
@@ -1,1 +1,13 @@
+int findMax(const vector<int>& arr) {
+int max = arr[0];//count=2,Array Access arr[0] + max = arr[0]
+for (int i = 1; i < arr.size(); i++) {//count=2
+if (arr[i] > max) { //count=2
+max = arr[i]; //count=2
+}
+return max;//count=1
+}
+//// Total operations:
+// 3 (initial) + n * 6 (loop operations) + 1 (return)
+// = 4 + 6n operations
+// Therefore, O(n) complexity
 
